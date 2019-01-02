62728bca5b1dce38d638e216487dff22675c3508
None <fake@fake.com>
2019-01-02 20:45:48 +0000

    Added parameter check.

--- game_logic/snakeMoveState.js
+++ game_logic/snakeMoveState.js
@@ -8,6 +8,20 @@
 {
     babylonProject.snakeMoveState = function ( babylon, gameData )
     {
+        if ( gameData == undefined )
+        {
+            throw new Error ( "gameData is undefined." );
+        }
+
+        if ( babylon == undefined )
+        {
+            throw new Error ( "babylon is undefined." );
+        }
+
+        if ( gameData.snakeParts == undefined )
+        {
+            throw new Error ( "gameData.snakeParts is undefined." );
+        }
     }
 
 } ( window.babylonProject = window.babylonProject || {} ));
--- game_logic/snakeMoveState.test.js
+++ game_logic/snakeMoveState.test.js
@@ -12,6 +12,14 @@ const startState = require ( "./snakeMoveState" );
  * MOCK DATA
  ***************************************************************************/
 
+let MockBabylon = jest.fn ();
+
+let MockGameData = jest.fn (
+    function ()
+    {
+        this.snakeParts = [];
+    });
+
 /****************************************************************************
  * TESTS
  ***************************************************************************/
@@ -22,5 +30,29 @@ describe ( "window.babylonProject.snakeMoveState", () =>
     {
         expect ( window.babylonProject.snakeMoveState )
             .toBeDefined ();
+    });
+
+    test ( "expects game data and babylon args to be defined", () =>
+    {
+        mock_babylon = new MockBabylon ();
+        mock_gameData = new MockGameData ();
+
+        expect ( () => 
+                window.babylonProject.snakeMoveState ( mock_babylon ))
+            .toThrow ( "gameData is undefined" );
+
+
+        expect ( () => 
+                window.babylonProject.snakeMoveState ( 
+                    undefined, mock_gameData ))
+            .toThrow ( "babylon is undefined" );
+
+        mock_gameData = new MockGameData ();
+        mock_gameData.snakeParts = undefined;
+
+        expect ( () =>
+                window.babylonProject.snakeMoveState (
+                    mock_babylon, mock_gameData ))
+            .toThrow ( "gameData.snakeParts is undefined" );
     });
 });
