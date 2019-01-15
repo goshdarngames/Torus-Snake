d8556a00190324097fb4f034178d1f9f1c01cf36
None <fake@fake.com>
2019-01-15 12:11:42 +0000

    Renamed files.

--- game_logic/torusCoordinates.js
+++ game_logic/torusCoordinates.js
@@ -3,12 +3,25 @@
  *
  * Provides a function that maps a set of 1 dimensional list indicies
  * to a two dimensional { x , y } coordinate tuple.
+ *
+ * The tests for the module have some examples of the functions with
+ * various shapes of array
  ***************************************************************************/
 
 ( function ( babylonProject, undefined )
 {
-    babylonProject.listIdxToCoord = function ( idx, width, height )
+    babylonProject.listIdxToCoord = function ( idx, width )
     {
+        if ( idx < 0 )
+        {
+            throw ( "idx must be >= 0" );
+        }
+
+        if ( width < 1 )
+        {
+            throw ( "width must be >= 0" );
+        }
+
         let coord = 
         { 
             x : idx % width,
@@ -17,5 +30,10 @@
 
         return coord;
     }
+    
+    babylonProject.coordToListIdx = function ( coord, width )
+    {
+        return ( width * coord.y ) + coord.x;
+    };
 
 } ( window.babylonProject = window.babylonProject || {} ));
--- game_logic/torusCoordinates.test.js
+++ game_logic/torusCoordinates.test.js
@@ -37,16 +37,22 @@ let listD = [ 0,
 let testCallData = [
 
     //first index
-    { idx : 0, width : 3, height : 3, list : listA, coord : { x:0, y:0 }},
-    { idx : 0, width : 2, height : 5, list : listB, coord : { x:0, y:0 }},
-    { idx : 0, width : 3, height : 1, list : listC, coord : { x:0, y:0 }},
-    { idx : 0, width : 1, height : 3, list : listD, coord : { x:0, y:0 }},
+    { idx : 0, width : 3, list : listA, coord : { x:0, y:0 }},
+    { idx : 0, width : 2, list : listB, coord : { x:0, y:0 }},
+    { idx : 0, width : 3, list : listC, coord : { x:0, y:0 }},
+    { idx : 0, width : 1, list : listD, coord : { x:0, y:0 }},
 
     //middle index
-    { idx : 4, width : 3, height : 3, list : listA, coord : { x:1, y:1 }},
-    { idx : 4, width : 2, height : 5, list : listB, coord : { x:0, y:2 }},
-    { idx : 1, width : 3, height : 1, list : listC, coord : { x:1, y:0 }},
-    { idx : 1, width : 1, height : 3, list : listD, coord : { x:0, y:1 }}
+    { idx : 4, width : 3, list : listA, coord : { x:1, y:1 }},
+    { idx : 4, width : 2, list : listB, coord : { x:0, y:2 }},
+    { idx : 1, width : 3, list : listC, coord : { x:1, y:0 }},
+    { idx : 1, width : 1, list : listD, coord : { x:0, y:1 }},
+
+    //final index
+    { idx : 8, width : 3, list : listA, coord : { x:2, y:2 }},
+    { idx : 9, width : 2, list : listB, coord : { x:1, y:4 }},
+    { idx : 2, width : 3, list : listC, coord : { x:2, y:0 }},
+    { idx : 2, width : 1, list : listD, coord : { x:0, y:2 }},
 ];
 
 /****************************************************************************
@@ -68,13 +74,48 @@ describe ( "window.babylonProject.listIdxToCoord", () =>
         {
             let idx = testData.idx;
             let width = testData.width;
-            let height = testData.height;
 
             let ret = 
-                window.babylonProject.listIdxToCoord ( idx, width, height );
+                window.babylonProject.listIdxToCoord ( idx, width );
 
             expect ( ret.x ).toBe ( testData.coord.x );
             expect ( ret.y ).toBe ( testData.coord.y );
+        });
+    });
+
+    test ( "test function with out of bounds data", () =>
+    {
+        expect ( () =>
+                window.babylonProject.listIdxToCoord ( -1, 2 ) )
+            .toThrow ( "idx must be >= 0" );
+
+        expect ( () =>
+                window.babylonProject.listIdxToCoord ( 1, 0 ) )
+            .toThrow ( "width must be >= 0" );
+
+    });
+});
+
+describe ( "window.babylonProject.coordToListIdx", () =>
+{
+    test ( "is defined", () =>
+    {
+        expect ( window.babylonProject.coordToListIdx )
+            .toBeDefined ();
+    });
+
+    test ( "test function with valid paramters from testCallData", () =>
+    {
+        testCallData.forEach ( function ( testData )
+        {
+            let coord = testData.coord;
+            let width = testData.width;
+
+            let ret = 
+                window.babylonProject.coordToListIdx ( coord, width );
+
+            expect ( ret ).toBe ( testData.idx );
+
         });
     });
 });
