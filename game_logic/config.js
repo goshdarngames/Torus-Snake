c6ab655f885f715a8195a32770b93c7b106791c6
None <fake@fake.com>
2019-01-28 13:02:41 +0000

    Created configuration file.

---
+++ game_logic/config.js
@@ -0,0 +1,17 @@
+/****************************************************************************
+ * config.js
+ *
+ * Provides access to configuration data.  Things such as object sizes,
+ * colours and positions can be defined here so they can be adjusted 
+ * during development.
+ ***************************************************************************/
+
+( function ( babylonProject, undefined )
+{
+
+    babylonProject.config = 
+    {
+        upPos : { x : 0, y : 1, z : 1 }
+    };
+
+} ( window.babylonProject = window.babylonProject || {} ));
---
+++ game_logic/config.test.js
@@ -0,0 +1,76 @@
+const test_module = require ("./config");
+
+/****************************************************************************
+ * config.test.js
+ *
+ * Provides tests for the configuration data.  It should not specify the
+ * exact values for the data but can provide some sanity checks.
+ ***************************************************************************/
+
+/****************************************************************************
+ * EXPECTED TEST VALUES
+ ***************************************************************************/
+
+/****************************************************************************
+ * MOCK DATA
+ ***************************************************************************/
+
+/****************************************************************************
+ * SETUP / TEARDOWN
+ ***************************************************************************/
+
+beforeEach ( () =>
+{
+});
+
+//Tests can assign a function here to have it called after they exit
+let oneTimeCleanUp = () => {};
+
+afterEach ( () =>
+{
+    //execute the one time cleanup and then set it as an empty function
+    //again
+ 
+    oneTimeCleanUp ();
+
+    oneTimeCleanUp  = () => {};
+});
+
+/****************************************************************************
+ * TESTS
+ ***************************************************************************/
+
+describe ( "window.babylonProject.config", () =>
+{
+    test ( "is defined", () =>
+    {
+        expect ( window.babylonProject.config )
+            .toBeDefined ();
+    });
+
+    test ( "defines upPos", () =>
+    {
+        expect ( window.babylonProject.config.upPos )
+            .toBeDefined ();
+
+        expect ( window.babylonProject.config.upPos.x )
+            .toBeDefined ();
+
+        expect ( parseFloat ( window.babylonProject.config.upPos.x ) )
+            .not.toBeNaN ();
+
+        expect ( window.babylonProject.config.upPos.y )
+            .toBeDefined ();
+
+        expect ( parseFloat ( window.babylonProject.config.upPos.y ) )
+            .not.toBeNaN ();
+
+        expect ( window.babylonProject.config.upPos.x )
+            .toBeDefined ();
+
+        expect ( parseFloat ( window.babylonProject.config.upPos.z ) )
+            .not.toBeNaN ();
+
+    });
+
+});
