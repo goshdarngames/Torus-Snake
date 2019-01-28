e5a9854640c78197288a43e0c930974270b2b2f8
None <fake@fake.com>
2019-01-28 15:52:58 +0000

    Created gui.js

---
+++ game_logic/base_game/gui.js
@@ -0,0 +1,14 @@
+/****************************************************************************
+ * gui.js
+ *
+ * Handles the creation of commonly used GUI components.
+ ***************************************************************************/
+
+( function ( babylonProject, undefined )
+{
+
+    babylonProject.createButtonPlane = function ()
+    {
+    };
+
+} ( window.babylonProject = window.babylonProject || {} ));
